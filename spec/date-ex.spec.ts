2d47d4cdcd51cd1b092980e23c26db0e852db34e
han41858 <han41858@gmail.com>
2020-10-25 18:47:48 +0900

    string initializer spec adaded

--- spec/date-ex.spec.ts
+++ spec/date-ex.spec.ts
@@ -28,170 +28,228 @@ describe('DateEx', () => {
 			expect(newDate.ms).to.be.closeTo(now.getMilliseconds(), MilliSecondsCloseTo);
 		});
 
-		// describe('by string', () => {
-		// 	describe('by date string', () => {
-		// 		describe('ok', () => {
-		// 			it('YYYY-MM-DD', () => {
-		// 				const now : Date = new Date();
-		//
-		// 				const dateStr : string = [
-		// 					now.getFullYear(),
-		// 					padDigit(now.getMonth() + 1, 2),
-		// 					padDigit(now.getDate(), 2)
-		// 				].join('-');
-		//
-		// 				const newDate : Date = new Date(dateStr);
-		// 				const timezoneOffsetInHours : number = now.getTimezoneOffset() / 60;
-		//
-		// 				const newDateEx : DateEx = new DateEx(dateStr);
-		//
-		// 				expect(newDateEx).to.be.ok;
-		//
-		// 				expect(+newDateEx).to.be.eql(+newDate);
-		//
-		// 				expect(newDateEx.year).to.be.eql(newDate.getFullYear());
-		// 				expect(newDateEx.month).to.be.eql(newDate.getMonth() + 1);
-		// 				expect(newDateEx.date).to.be.eql(newDate.getDate());
-		//
-		// 				expect(newDateEx.hours).to.be.eql(-timezoneOffsetInHours);
-		// 				expect(newDateEx.minutes).to.be.eql(0);
-		// 				expect(newDateEx.seconds).to.be.eql(0);
-		// 				expect(newDateEx.ms).to.be.eql(0);
-		// 			});
-		//
-		// 			it('YYYY-MM', () => {
-		// 				const now : Date = new Date();
-		//
-		// 				const dateStr : string = [
-		// 					now.getFullYear(),
-		// 					padDigit(now.getMonth() + 1, 2)
-		// 				].join('-');
-		//
-		// 				const newDate : Date = new Date(dateStr);
-		// 				const timezoneOffsetInHours : number = now.getTimezoneOffset() / 60;
-		//
-		// 				const newDateEx : DateEx = new DateEx(dateStr);
-		//
-		// 				expect(newDateEx).to.be.ok;
-		//
-		// 				expect(+newDateEx).to.be.eql(+newDate);
-		//
-		// 				expect(newDateEx.year).to.be.eql(newDate.getFullYear());
-		// 				expect(newDateEx.month).to.be.eql(newDate.getMonth() + 1);
-		// 				expect(newDateEx.date).to.be.eql(1);
-		//
-		// 				expect(newDateEx.hours).to.be.eql(-timezoneOffsetInHours);
-		// 				expect(newDateEx.minutes).to.be.eql(0);
-		// 				expect(newDateEx.seconds).to.be.eql(0);
-		// 				expect(newDateEx.ms).to.be.eql(0);
-		// 			});
-		// 		});
-		//
-		// 		describe('failed', () => {
-		// 			// YYYYMM assumes hhmmss
-		//
-		// 			it('YYYYMMDD', () => {
-		// 				const now : Date = new Date();
-		//
-		// 				const dateStr : string = [
-		// 					now.getFullYear(),
-		// 					padDigit(now.getMonth() + 1, 2),
-		// 					padDigit(now.getDate(), 2)
-		// 				].join('');
-		//
-		//
-		// 				expect(() => new DateEx(dateStr)).throws();
-		// 			});
-		// 		});
-		//
-		// 		// TODO:
-		// 		// --MM-DD
-		// 		// --MMDD[1]
-		// 	});
-		//
-		// 	xdescribe('by time string', () => {
-		// 		describe('no timezone', () => {
-		// 			// TODO:
-		// 			// hh:mm:ss.sss
-		// 			// Thhmmss.sss
-		// 			// hh:mm:ss
-		// 			// Thhmmss
-		// 			// hh:mm
-		// 			// Thhmm
-		// 			// Thh
-		// 		});
-		//
-		// 		describe('with timezone', () => {
-		// 			// TODO:
-		// 			// <time>Z
-		// 			// <time>±hh:mm
-		// 			// <time>±hhmm
-		// 			// <time>±hh
-		//
-		// 			it('YYYY-MM-DDTHH:mm:ssZ', () => {
-		// 				const now : Date = new Date();
-		//
-		// 				const newDate : Date = new Date(now);
-		//
-		// 				const dateStr : string = [
-		// 					newDate.getFullYear(),
-		// 					padDigit(newDate.getMonth() + 1, 2),
-		// 					padDigit(newDate.getDate(), 2)
-		// 				].join('-');
-		//
-		// 				const timeStr : string = [
-		// 					padDigit(newDate.getHours(), 2),
-		// 					padDigit(newDate.getMinutes(), 2),
-		// 					padDigit(newDate.getSeconds(), 2)
-		// 				].join(':');
-		//
-		// 				const newDateEx : DateEx = new DateEx(`${ dateStr }T${ timeStr }Z`);
-		//
-		// 				expect(newDateEx).to.be.ok;
-		//
-		// 				expect(newDateEx.year).to.be.eql(newDate.getFullYear());
-		// 				expect(newDateEx.month).to.be.eql(newDate.getMonth() + 1);
-		// 				expect(newDateEx.date).to.be.eql(newDate.getDate());
-		//
-		// 				expect(newDateEx.hours).to.be.eql(newDate.getHours());
-		// 				expect(newDateEx.minutes).to.be.eql(newDate.getMinutes());
-		// 				expect(newDateEx.seconds).to.be.eql(newDate.getSeconds());
-		// 				expect(newDateEx.ms).to.be.eql(newDate.getMilliseconds());
-		// 			});
-		//
-		// 			it('YYYY-MM-DDTHH:mm:ss.SSSZ', () => {
-		// 				const now : Date = new Date();
-		//
-		// 				const newDate : Date = new Date(now);
-		//
-		// 				const dateStr : string = [
-		// 					newDate.getFullYear(),
-		// 					padDigit(newDate.getMonth() + 1, 2),
-		// 					padDigit(newDate.getDate(), 2)
-		// 				].join('-');
-		//
-		// 				const timeStr : string = [
-		// 					padDigit(newDate.getHours(), 2),
-		// 					padDigit(newDate.getMinutes(), 2),
-		// 					padDigit(newDate.getSeconds(), 2)
-		// 				].join(':') + `.${ padDigit(newDate.getMilliseconds(), 3) }`;
-		//
-		// 				const newDateEx : DateEx = new DateEx(`${ dateStr }T${ timeStr }Z`);
-		//
-		// 				expect(newDateEx).to.be.ok;
-		//
-		// 				expect(newDateEx.year).to.be.eql(newDate.getFullYear());
-		// 				expect(newDateEx.month).to.be.eql(newDate.getMonth() + 1);
-		// 				expect(newDateEx.date).to.be.eql(newDate.getDate());
-		//
-		// 				expect(newDateEx.hours).to.be.eql(newDate.getHours());
-		// 				expect(newDateEx.minutes).to.be.eql(newDate.getMinutes());
-		// 				expect(newDateEx.seconds).to.be.eql(newDate.getSeconds());
-		// 				expect(newDateEx.ms).to.be.eql(newDate.getMilliseconds());
-		// 			});
-		// 		});
-		// 	});
-		// });
+		// test string initializer
+		describe.only('by string', () => {
+			describe('by date string', () => {
+				describe('ok', () => {
+					it('YYYY-MM-DD', () => {
+						const now : Date = new Date();
+
+						const dateStr : string = [
+							now.getFullYear(),
+							padDigit(now.getMonth() + 1, 2),
+							padDigit(now.getDate(), 2)
+						].join('-');
+
+						const newDate : Date = new Date(dateStr);
+						const timezoneOffsetInHours : number = now.getTimezoneOffset() / 60;
+
+						const newDateEx : DateEx = new DateEx(dateStr);
+
+						expect(newDateEx).to.be.ok;
+
+						expect(+newDateEx).to.be.eql(+newDate);
+
+						expect(newDateEx.year).to.be.eql(newDate.getFullYear());
+						expect(newDateEx.month).to.be.eql(newDate.getMonth() + 1);
+						expect(newDateEx.date).to.be.eql(newDate.getDate());
+
+						expect(newDateEx.hours).to.be.eql(-timezoneOffsetInHours);
+						expect(newDateEx.minutes).to.be.eql(0);
+						expect(newDateEx.seconds).to.be.eql(0);
+						expect(newDateEx.ms).to.be.eql(0);
+					});
+
+					it('YYYY-MM', () => {
+						const now : Date = new Date();
+
+						const dateStr : string = [
+							now.getFullYear(),
+							padDigit(now.getMonth() + 1, 2)
+						].join('-');
+
+						const newDate : Date = new Date(dateStr);
+						const timezoneOffsetInHours : number = now.getTimezoneOffset() / 60;
+
+						const newDateEx : DateEx = new DateEx(dateStr);
+
+						expect(newDateEx).to.be.ok;
+
+						expect(+newDateEx).to.be.eql(+newDate);
+
+						expect(newDateEx.year).to.be.eql(newDate.getFullYear());
+						expect(newDateEx.month).to.be.eql(newDate.getMonth() + 1);
+						expect(newDateEx.date).to.be.eql(1);
+
+						expect(newDateEx.hours).to.be.eql(-timezoneOffsetInHours);
+						expect(newDateEx.minutes).to.be.eql(0);
+						expect(newDateEx.seconds).to.be.eql(0);
+						expect(newDateEx.ms).to.be.eql(0);
+					});
+
+					it('--MM-DD', () => {
+						const now : Date = new Date();
+
+						const dateStr : string = [
+							'--',
+							padDigit(now.getMonth(), 2), // not +1
+							'-',
+							padDigit(now.getDate(), 2)
+						].join('');
+
+						const newDate : Date = new Date(dateStr);
+
+						const newDateEx : DateEx = new DateEx(dateStr);
+
+						expect(newDateEx).to.be.ok;
+
+						expect(+newDateEx).to.be.eql(+newDate);
+
+						expect(newDateEx.year).to.be.eql(newDate.getFullYear());
+						expect(newDateEx.month).to.be.eql(newDate.getMonth() + 1);
+						expect(newDateEx.date).to.be.eql(newDate.getDate());
+
+						expect(newDateEx.hours).to.be.eql(0);
+						expect(newDateEx.minutes).to.be.eql(0);
+						expect(newDateEx.seconds).to.be.eql(0);
+						expect(newDateEx.ms).to.be.eql(0);
+					});
+				});
+
+				describe('failed', () => {
+					// YYYYMM assumes hhmmss
+
+					it('YYYYMMDD', () => {
+						const now : Date = new Date();
+
+						const dateStr : string = [
+							now.getFullYear(),
+							padDigit(now.getMonth() + 1, 2),
+							padDigit(now.getDate(), 2)
+						].join('');
+
+						expect(new DateEx(dateStr).isValid()).to.be.false;
+					});
+				});
+			});
+
+			describe('by time string', () => {
+				describe('no timezone', () => {
+					describe('failed', () => {
+						it('hh:mm:ss.SSS', () => {
+							const now : Date = new Date();
+
+							const timeStr : string = [
+								padDigit(now.getHours(), 2),
+								':',
+								padDigit(now.getMinutes(), 2),
+								':',
+								padDigit(now.getSeconds(), 2),
+								'.',
+								padDigit(now.getMilliseconds(), 3)
+							].join('');
+
+							const dateEx : DateEx = new DateEx(timeStr);
+
+							expect(dateEx.isValid()).to.be.false;
+						});
+
+						it('Thh:mm:ss.SSS', () => {
+							const now : Date = new Date();
+
+							const timeStr : string = [
+								'T',
+								padDigit(now.getHours(), 2),
+								':',
+								padDigit(now.getMinutes(), 2),
+								':',
+								padDigit(now.getSeconds(), 2),
+								'.',
+								padDigit(now.getMilliseconds(), 3)
+							].join('');
+
+							const dateEx : DateEx = new DateEx(timeStr);
+
+							expect(dateEx.isValid()).to.be.false;
+						});
+
+						it('hh:mm:ss', () => {
+							const now : Date = new Date();
+
+							const timeStr : string = [
+								padDigit(now.getHours(), 2),
+								':',
+								padDigit(now.getMinutes(), 2),
+								':',
+								padDigit(now.getSeconds(), 2)
+							].join('');
+
+							const dateEx : DateEx = new DateEx(timeStr);
+
+							expect(dateEx.isValid()).to.be.false;
+						});
+
+						// hhmmss : not error, but invalid parsed with Date
+
+						it('Thhmmss', () => {
+							const now : Date = new Date();
+
+							const timeStr : string = [
+								'T',
+								padDigit(now.getHours(), 2),
+								padDigit(now.getMinutes(), 2),
+								padDigit(now.getSeconds(), 2)
+							].join('');
+
+							const dateEx : DateEx = new DateEx(timeStr);
+
+							expect(dateEx.isValid()).to.be.false;
+						});
+
+						it('hh:mm', () => {
+							const now : Date = new Date();
+
+							const timeStr : string = [
+								padDigit(now.getHours(), 2),
+								':',
+								padDigit(now.getMinutes(), 2)
+							].join('');
+
+							const dateEx : DateEx = new DateEx(timeStr);
+
+							expect(dateEx.isValid()).to.be.false;
+						});
+
+						it('Thhmm', () => {
+							const now : Date = new Date();
+
+							const timeStr : string = [
+								'T',
+								padDigit(now.getHours(), 2),
+								padDigit(now.getMinutes(), 2)
+							].join('');
+
+							const dateEx : DateEx = new DateEx(timeStr);
+
+							expect(dateEx.isValid()).to.be.false;
+						});
+
+						it('Thh', () => {
+							const now : Date = new Date();
+
+							const timeStr : string = [
+								'T',
+								padDigit(now.getHours(), 2)
+							].join('');
+
+							const dateEx : DateEx = new DateEx(timeStr);
+
+							expect(dateEx.isValid()).to.be.false;
+						});
+					});
+				});
+			});
+		});
 
 		describe('with Date', () => {
 			it('ok', () => {

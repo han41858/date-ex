81cb4bbc6d71b1ad7463b526b2ca7a348e0d47de
han41858 <han41858@gmail.com>
2021-05-06 22:53:14 +0900

    DurationUnit added to public-api

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,5 +2,5 @@ export { DateTime } from './date-time';
 export { Duration } from './duration';
 
 export { DateTimeParam, InitDataFormat, DurationParam, YearCalendar, MonthCalendar } from './interfaces';
-export { DateTimeUnit, FormatToken } from './constants';
+export { DateTimeUnit, FormatToken, DurationUnit } from './constants';
 export { dateFormat } from './util';
